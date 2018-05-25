19f8d746fa10eddeb9af4026f6eda9bf6ff8127f
HCMUS-Smart-Environment-Group <smartenvironment@selab.hcmus.edu.vn>
2018-05-25 15:17:55 +0700

    Adding path

--- src/config.py
+++ src/config.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+# COMMON CONFIG
+
+DTYPE_FLOAT = np.float32
+
 #PATH CONFIG
 
 IMAGE_DIR = '/Users/truongthanhdat/Dataset/LSC/Image'
@@ -11,7 +17,12 @@ DATABASE_DIR = '/Users/truongthanhdat/Dataset/LSC/Database'
 PIXEL_THRESHOLD = 0.2
 VOTE_THRESHOLD = 0.5
 
-# PRETRAINED MODEL
-
+# WORD2VEC MODEL
 WORD2VEC_MODEL = '../Database/GoogleNews-vectors-negative300.bin'
+TOP_WORD_RETRIEVAL = 10
+WORD_RETRIEVAL_THRESHOLD = 0.5
+DICTIONARY_PATH = '../Database/dictionary.npy'
 
+# KD-Tree
+KD_TREE_INDEX_PATH = '../Database/kd_tree_index.flann'
+DATA_KD_TREE_PATH = '../Database/kd_tree_data.npy'
